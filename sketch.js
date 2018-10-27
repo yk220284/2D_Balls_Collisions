73cd45ec2a48ba60a2002d8ad5c8acdb3b572e33
Yankang <Yankang@MacdeMacBook-Air.local>
2018-10-27 22:50:18 +0100

    added new trace feature

--- sketch.js
+++ sketch.js
@@ -1,39 +1,63 @@
 let balls = [];
-let num_of_balls = 3;
+let num_of_balls = 150;
+let pause = false;
+let trace = false;
 
-function createBall(ox,oy){
-	let sx = random(-2,2);
-	let sy = random(-2,2);
-	let r = random(20,60);
-	nb = new BouncyBall(ox,oy,sx,sy,r);
-	balls.push(nb);
-	if (balls.length > 10) {
-		balls.splice(0,1);
+// All key effects
+function keyPressed() {
+	if (keyCode == 84) { // t for trace
+		clearPath();
+		trace = !trace;
+		//clean up the earlier mess
+	} else if (keyCode == 80) { // p for pause
+		pause = !pause;
+	} else if (keyCode == 67) { // c for clearing the path
+		clearPath();
 	}
 }
+// Throw small particles 
+function createBall(ox, oy) {
+	let sx = random(-10, 10);
+	let sy = random(-10, 10);
+	let r = random(2, 4);
+	nb = new BouncyBall(ox, oy, sx, sy, r);
+	balls.push(nb);
+	if (balls.length > 1000) {
+		balls.splice(0, 1);
+	}
+}
+var canvas
 
 function setup() {
-	createCanvas(window.innerWidth, window.innerHeight);
-	background(0);
-}
-
-window.addEventListener( 'resize' , function() {
-	createCanvas(window.innerWidth, window.innerHeight);
-	background(0);
+	canvas = createCanvas(400, 300);
+	canvas.parent('sketch-holder');
+	for (let i = 0; i < num_of_balls; i++) {
+		createBall(Math.random() * canvas.width, Math.random() * canvas.height);
 	}
-)
-
-function doubleClicked(){
-	let ox = mouseX;
-	let oy = mouseY;
-	createBall(ox,oy);
 }
 
+
+// mouse effect
+function doubleClicked() {
+	let p = canvas.position();
+	if ((0 < mouseX && mouseX < canvas.width && 0 < mouseY && mouseY < canvas.height)) {
+		let sx = 0;
+		let sy = 0;
+		let r = random(30, 40);
+		nb = new BigBall(mouseX, mouseY, sx, sy, r);
+		balls.push(nb);
+		if (balls.length > 1000) {
+			balls.splice(0, 1);
+		}
+	}
+}
+
+// handle collisions
 function draw() {
 	background(0);
-	for (var i = 0; i < balls.length ; i++) {
-		for (var j = 0; j < balls.length ; j++) {
-			if (balls[i]!==balls[j] && balls[i].collide_soon(balls[j])){
+	for (var i = 0; i < balls.length; i++) {
+		for (var j = 0; j < balls.length; j++) {
+			if (balls[i] !== balls[j] && balls[i].collide_soon(balls[j])) {
 				console.log('collide')
 				balls[i].bounceOff(balls[j]);
 				balls[i].staticCollision(balls[j]);
@@ -41,22 +65,39 @@ function draw() {
 		};
 	}
 
-
-
 	for (var i = 0; i < balls.length; i++) {
-		balls[i].move();
 		balls[i].show();
 		balls[i].bounce();
 	}
-
+	// pausing
+	if (pause == false) {
+		for (let i = 0; i < balls.length; i++) {
+			balls[i].move();
+		}
+	}
 
 }
 
-function mousePressed(){
-	for (var i = balls.length-1; i >= 0; i--) {
-		if (balls[i].contain(mouseX,mouseY)) {
-			console.log('deleted'+' '+i)
-			balls.splice(i,1);
+
+function clearPath() {
+	// tracing
+	for (let i = num_of_balls; i < balls.length; i++) {
+		balls[i].history = [];
+	}
+}
+
+function mousePressed() {
+	for (var i = balls.length - 1; i >= 0; i--) {
+		if (balls[i].contain(mouseX, mouseY)) {
+			console.log('deleted' + ' ' + i)
+			balls.splice(i, 1);
 		}
+	}
+}
+
+function applyDrag() {
+	for (var i = 0; i < balls.length; i++) {
+		balls[i].speedx *= 0.5;
+		balls[i].speedy *= 0.5;
 	}
 }
